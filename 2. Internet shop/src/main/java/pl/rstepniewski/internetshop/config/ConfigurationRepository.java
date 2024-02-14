3e2d04cdca84ce16cac0653f2290c393b3d0cded
Rafal Stepniewski <vultur12@gmail.com>
2024-02-14 19:48:47 +0100

    delete redundant classes

--- 2. Internet shop/src/main/java/pl/rstepniewski/internetshop/config/ConfigurationRepository.java
+++
@@ -1,8 +0,0 @@
-package pl.rstepniewski.internetshop.config;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConfigurationRepository extends CrudRepository<Configuration, String> {
-}
