ea49c455d870baa0025aaf37b67f37bfdfb7463e
Rafal Stepniewski <vultur12@gmail.com>
2024-03-20 08:31:59 +0100

    initial setup

---
+++ 4. Simple Login System/src/test/java/pl/rstepniewski/loginpage/LoginPageApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rstepniewski.loginpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginPageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
